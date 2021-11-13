3fc8a20d06f6ccd9b9d310f448d2dad22a88d18e
yamamoto <kyoyamamoto1202@gmail.com>
2021-11-13 13:38:08 +0900

    Initial Commit

---
+++ AutomatingAssets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutomatingAssets
+//
+//  Created by 山本響 on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
